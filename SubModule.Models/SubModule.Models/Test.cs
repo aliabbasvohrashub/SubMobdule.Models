fa43ab11390940e9e14da157ae5a097781ac7a2b
Ali <avohra@elevatesaas.com>
2021-07-09 12:35:03 +0530

    ìnitial commit

---
+++ SubModule.Models/SubModule.Models/Test.cs
@@ -0,0 +1,9 @@
+﻿namespace SubModule.Models
+{
+    public class Test
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Address { get; set; }
+    }
+}
